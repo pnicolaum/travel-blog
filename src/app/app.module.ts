6637efee497f2bbeb7d9f120489556eefde24754
pnicolaum <p@nicolau.com>
2023-11-05 18:46:27 +0100

    creacion componentes pag principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './core/components/contact-form/contact-form.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { SliderComponent } from './core/components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SliderComponent,
+    ContactFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
