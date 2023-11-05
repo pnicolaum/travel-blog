3d1e9f059a2e28d2b3b46ea038cecf6c0ecbfc72
pnicolaum <149788686+pnicolaum@users.noreply.github.com>
2023-11-05 18:52:10 +0100

    Revert "Tb 11"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactFormComponent } from './core/components/contact-form/contact-form.component';
-import { FooterComponent } from './core/components/footer/footer.component';
-import { HeaderComponent } from './core/components/header/header.component';
-import { SliderComponent } from './core/components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    SliderComponent,
-    ContactFormComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
