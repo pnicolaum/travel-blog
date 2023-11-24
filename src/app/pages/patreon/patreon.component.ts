f278d6494e3601b967addc02a68891ea882f0dfa
pnicolaum <p@nicolau.com>
2023-11-24 19:54:17 +0100

    patreon page

--- src/app/pages/patreon/patreon.component.ts
+++ src/app/pages/patreon/patreon.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-patreon',
   templateUrl: './patreon.component.html',
-  //styleUrls: ['./patreon.component.css']
+  styleUrls: ['./patreon.component.css']
 })
 export class PatreonComponent {
   title = 'travel-blog';
