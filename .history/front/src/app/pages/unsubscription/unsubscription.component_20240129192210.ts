5ac739bfeaed889f706a5cffeec61ee1bc04c442
pnicolaum <p@nicolau.com>
2024-01-29 23:32:47 +0100

    image front message, footer, hero

---
+++ .history/front/src/app/pages/unsubscription/unsubscription.component_20240129192210.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { SubscriptionService } from 'src/app/core/services/subscription/subscription.service';
+
+
+@Component({
+  selector: 'app-unsubscription',
+  templateUrl: './unsubscription.component.html',
+  styleUrls: ['./unsubscription.component.css']
+})
+
+export class UnsubscriptionComponent {
+  mail = "";
+
+  isModalOpen: boolean = false;
+  mensaje = "";
+
+  constructor(
+    private subscriptionService: SubscriptionService
+  ) { }
+
+  deleteMail() {
+    // delete
+
+    if (this.mail) {
+      this.subscriptionService.deleteMail(this.mail)
+        .subscribe(
+          (response) => {
+            this.openModal();
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    }
+
+
+  };
+
+
+  openModal() {
+    this.isModalOpen = true;
+  };
+
+  closeModal() {
+    this.isModalOpen = false;
+  };
+
+  preventClose(event: Event) {
+    event.stopPropagation();
+  };
+}
---
+++ .history/front/src/app/pages/unsubscription/unsubscription.component_20240129192536.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { SubscriptionService } from 'src/app/core/services/subscription/subscription.service';
+
+
+@Component({
+  selector: 'app-unsubscription',
+  templateUrl: './unsubscription.component.html',
+  styleUrls: ['./unsubscription.component.css']
+})
+
+export class UnsubscriptionComponent {
+  mail = "";
+
+  isModalOpen: boolean = false;
+  mensaje = "";
+
+  constructor(
+    private subscriptionService: SubscriptionService
+  ) { }
+
+  deleteMail() {
+
+    this.subscriptionService.getMailByMail(this.mail)
+      .subscribe(
+        data => {
+          if (data.length > 0) {
+            this.subscriptionService.deleteMail(this.mail)
+              .subscribe(
+                (response) => {
+                  this.openModal();
+
+                  console.log(response);
+                },
+                (error) => {
+                  console.log(error);
+                }
+              );
+          } else {
+            this.mensaje = "No hemos encontrado ningun correo";
+          }
+        }
+      )
+
+
+    if (this.mail) {
+
+    } else {
+      this.openModal();
+      this.mensaje = "Por favor indique un correo"
+    }
+
+
+  };
+
+
+  openModal() {
+    this.isModalOpen = true;
+  };
+
+  closeModal() {
+    this.isModalOpen = false;
+  };
+
+  preventClose(event: Event) {
+    event.stopPropagation();
+  };
+}
