c35de097518ca4066fabb37516c5d1fc79006b40
pnicolaum <p@nicolau.com>
2024-01-30 14:34:19 +0100

    logo, patreon, about-us, footer

---
+++ .history/front/src/app/core/components/contact-form/contact-form.component_20240130104720.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { NodemailerService } from '../../services/nodemailer/nodemailer.service';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+  name = "";
+  mail = "";
+  text = "";
+
+  isModalOpen: boolean = false;
+  mensaje = ""
+
+  constructor(
+    private nodemailerFormService: NodemailerService
+  ) { }
+
+  postMail() {
+
+    const data = {
+      name: this.name,
+      mail: this.mail,
+      text: this.text,
+    };
+
+    if (this.name && this.mail && this.text) {
+      this.nodemailerFormService.postMail(data)
+        .subscribe(
+          response => {
+            // this.data = response;
+            console.log(response);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+
+
+    } else {
+      console.log('Algunos campos están vacíos, por favor, rellene todos los campos.');
+    }
+
+  };
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  preventClose(event: Event) {
+    event.stopPropagation();
+  }
+
+}
