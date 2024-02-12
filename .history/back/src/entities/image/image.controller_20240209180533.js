daa2f64d97a4a14b352f973150e9a2ae6522a978
pnicolaum <p@nicolau.com>
2024-02-12 11:58:11 +0100

    fixes

---
+++ .history/back/src/entities/image/image.controller_20240209180533.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const uploadImage = (req, res) => {
+    res.json({ message: 'Imagen subida con éxito' });
+};
+
+const getImages = (req, res) => {
+    const uploadDirectory = './uploads';
+
+    fs.readdir(uploadDirectory, (err, files) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const imageNames = files.map(file => path.basename(file));
+        res.status(200).json({ images: imageNames });
+    });
+};
+
+const deleteImage = (req, res) => {
+    const imageName = req.params.imageName;
+    const imagePath = path.join('./uploads', imageName);
+
+    fs.unlink(imagePath, (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error al eliminar la imagen' });
+        }
+
+        return res.json({ message: 'Imagen eliminada con éxito' });
+
+    });
+}
+
+module.exports = {
+    uploadImage,
+    getImages,
+    deleteImage
+};
---
+++ .history/front/src/app/core/services/auth/auth.service_20240207172010.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  private isAuthenticatedKey = 'isAuthenticated';
+  private sessionTimeout = 60 * 60 * 1000;  // 1 hora en milisegundos
+  // private sessionTimeout = 10 * 1000;  // 10 segundos en milisegundos
+  private sessionTimer: any;
+
+
+
+  setAuthenticated(value: boolean): void {
+    localStorage.setItem(this.isAuthenticatedKey, value.toString());
+  }
+
+  isAuthenticated(): boolean {
+    return localStorage.getItem(this.isAuthenticatedKey) === 'true';
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.isAuthenticatedKey);
+  }
+
+  startSessionTimer(): void {
+    this.clearSessionTimer();
+
+    this.sessionTimer = setTimeout(() => {
+      this.logout();
+      console.log("Sesión expirada");
+    }, this.sessionTimeout);
+
+    console.log("Temporizador iniciado");
+  }
+
+  clearSessionTimer(): void {
+    if (this.sessionTimer) {
+      clearTimeout(this.sessionTimer);
+    }
+  }
+
+}
